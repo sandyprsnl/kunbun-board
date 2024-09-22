209317429eaf66bb1bafb3f5b1f71ff94d14f6b7
sandeep bhardwaj <bsandeep650@gmail.com>
2024-09-22 15:08:52 +0530

    add kunbun board

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react';
+import TaskCard from './card/TaskCard';
+import { TaskData } from './taskData/TaskData';
 function App() {
+  var [taskData,setTaskData] = useState(TaskData());
+  var [dragClass,setDragClass] = useState(false);
+  const todoTasks = taskData.filter((task) => task.status === 'todo');
+  const inProgressTasks = taskData.filter((task) => task.status ==='in-progress');
+  const doneTasks = taskData.filter((task) => task.status === 'done');
+  const handleDrop =(e,status)=>{
+    e.preventDefault();
+    let tasks = taskData.map((task)=>{
+      if(task.id=== e.dataTransfer.getData('id')){
+        task.status=status;
+      }
+      return task;
+    });
+    setTaskData([...tasks]);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <div className='flex divide-y m-auto max-w-screen-md'>
+        <div className='border bg-red-400 p-2 m-2 rounded' onDragEnter={()=>{setDragClass(true)}} onDrop={(e)=>{handleDrop(e,"todo")}} onDragOver={(e)=>e.preventDefault()}>
+        <div className='flex justify-between'>        
+        <h1 className='font-bold  text-3xl capitalize text-gray-700'>ToDo</h1>
+        <h1 className='font-bold  text-3xl capitalize text-gray-700'>{todoTasks.length}</h1>
+        </div>
+          {
+            todoTasks.map((task ,index) => <TaskCard key={index} task={task} />)
+          }
+        </div>
+        <div className='border bg-yellow-400 p-2 m-2 rounded'  onDrop={(e)=>{handleDrop(e,'in-progress')}}  onDragOver={(e)=>e.preventDefault()}>
+        <div className='flex justify-between'>        
+        <h1 className='font-bold  text-3xl capitalize text-gray-700'>In Progress</h1>
+        <h1 className='font-bold  text-3xl capitalize text-gray-700'>{inProgressTasks.length}</h1>
+        </div>
+          {
+            inProgressTasks.map((task ,index) => <TaskCard key={index} task={task} />)
+          }
+        </div>
+        <div className='border bg-green-400 p-2 m-2 rounded'  onDrop={(e)=>{handleDrop(e,'done')}}  onDragOver={(e)=>e.preventDefault()}>
+        <div className='flex justify-between'>        
+        <h1 className='font-bold  text-3xl capitalize text-gray-700'>Done</h1>
+        <h1 className='font-bold  text-3xl capitalize text-gray-700'>{doneTasks.length}</h1>
+        </div>
+          {
+            doneTasks.map((task ,index) => <TaskCard key={index} task={task} />)
+          }
+        </div>
+      </div>
+
+    </section>
   );
 }
 
